4a37417ffb2c55882a0b8df9c6dcdeff1e409ce6
Rodrigo Aguiar <rodrigo_aguiars@outlook.com>
2021-09-30 19:11:21 -0300

    Complementando a classe ProductAdapter e Conclusão

--- app/src/main/java/com/rodrigo/applicationworkapi/MainActivity.kt
+++ app/src/main/java/com/rodrigo/applicationworkapi/MainActivity.kt
@@ -2,7 +2,14 @@ package com.rodrigo.applicationworkapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.rodrigo.applicationworkapi.api.MyRetrofit
+import com.rodrigo.applicationworkapi.model.Produto
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +20,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         recycleProdutos = findViewById(R.id.recycle_rodutos)
+        recycleProdutos.layoutManager = LinearLayoutManager(this)
+        getData()
+    }
+    private fun getData(){
+        val call: Call<List<Produto>> = MyRetrofit.instance?.produtoApi()?.getProdutoApi() as Call<List<Produto>>
+
+        call.enqueue(object: Callback<List<Produto>>{
+            override fun onResponse(call: Call<List<Produto>>, response: Response<List<Produto>>) {
+                val adapter = ProdutosAdapter(this@MainActivity, response.body()?.toList() as List<Produto>)
+
+                recycleProdutos.adapter = adapter
+            }
+
+            override fun onFailure(call: Call<List<Produto>>, t: Throwable) {
+                Toast.makeText(this@MainActivity, t.message, Toast.LENGTH_LONG).show()
+            }
+
+        })
     }
 }
--- app/src/main/java/com/rodrigo/applicationworkapi/ProdutosAdapter.kt
+++ app/src/main/java/com/rodrigo/applicationworkapi/ProdutosAdapter.kt
@@ -1,29 +1,39 @@
 package com.rodrigo.applicationworkapi
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.rodrigo.applicationworkapi.model.Produto
 
 class ProdutosAdapter(
     private val context: Context,
     private val produtoList: List<Produto>
     , ): RecyclerView.Adapter<ProdutosViewHolder>(){
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProdutosViewHolder {
-        TODO("Not yet implemented")
-    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProdutosViewHolder =
+        ProdutosViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.produto_item, parent, false))
+
 
     override fun onBindViewHolder(holder: ProdutosViewHolder, position: Int) {
-        TODO("Not yet implemented")
+
+        val item = produtoList[position]
+        holder.produtoNome.text = item.prName
+        holder.produtoPreco.text = item.prPrice
+        Glide.with(context).load(item.prImage).into(holder.produtoImage)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = produtoList.size
 
 }
 
 class ProdutosViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+    val produtoImage: ImageView = itemView.findViewById(R.id.imgProduto)
+    val produtoNome: TextView = itemView.findViewById(R.id.nomeProduto)
+    val produtoPreco: TextView = itemView.findViewById(R.id.precoProduto)
 
 }
